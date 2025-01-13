1e83504a33f983d5918b5eb1489a33c4c9922710
mandilorenzo <amanda.lorenzo@hotmail.com>
2025-01-13 19:07:31 -0300

    ajustes

--- js/index.js
+++ js/index.js
@@ -1,30 +1,25 @@
-const inputText = document.querySelectorAll('input')
+const inputText = document.querySelectorAll('.campo-obrigatorio')
+const formulario = document.querySelector('.form')
+const camposObrigatorios = document.querySelectorAll('.campo-obrigatorio')
 
 inputText.forEach(input => {
     input.addEventListener('input', () => {
         if (input.value !== "") {
-            input.classList.add('form','borda-verde');
+            input.classList.add('borda-verde');
         } else {
-            input.classList.remove('form','borda-verde');
+            input.classList.remove('borda-verde');
         }
     })
 })
 
-
-const formulario = document.querySelector('.form')
-const camposObrigatorios = document.querySelectorAll('.campo-obrigatorio')
-
 formulario.addEventListener('submit', event => {
     event.preventDefault();
-
-    let formularioValido = true;
 
     camposObrigatorios.forEach((campo) => {
         const mensagemErro = campo.nextElementSibling;
         if(campo.value === "") {
             campo.classList.add('borda-vermelha');
             mensagemErro.textContent = 'campo obrigatório';
-            formularioValido = false;
         } else {
             campo.classList.remove('borda-vermelha');
             mensagemErro.textContent = "";
